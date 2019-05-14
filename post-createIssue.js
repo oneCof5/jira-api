c1e63916ddc7477c0bc350458673c77eaeee29e7
Chris Pearson <cpearson@silverchair.com>
2019-05-14 14:51:05 -0400

    updates

--- post-createIssue.js
+++ post-createIssue.js
@@ -1,25 +1,28 @@
 {
-    "update": {},
     "fields": {
         "project": {
             "id": "13280"
         },
-        "summary": "Staging/Production Validation in Sprint XXX (Katana BAU)",
+        "summary": "[PLACEHOLDER] Unplanned Work in PI 8 Planning Sprint - Katana BAU",
         "issuetype": {
             "id": "3"
         },
         "reporter": {
-            "name": "cpearson"
+            "name": "cpearson",
+            "key": "cpearson",
+            "emailAddress": "cpearson@silverchair.com"
         },
         "labels": [
-            "placeholder"
+            "placeholder",
+            "platform_other"
         ],
-        "description": "h2. Staging and Production post validation\r\nTiming expectations: https://confluence.silverchair.com/display/RM/Validation+expectations+for+scheduled+posts\r\n\r\nSCM6 Validation Board - Katana: \r\nhttps://jira.silverchair.com/secure/RapidBoard.jspa?rapidView=331&quickFilter=2122\r\n\r\n|| Environment/Post || Date || Automated Tests || Validation Complete || Notes ||\r\n| Staging R1 |  |  |  |  |\r\n| Staging R2 |  |  |  |  |\r\n| Production |  |  |  |  |",
+        "description": "h2. Placholder ticket\r\nPoints to be consumed by surprise work pulled into (or added to with work pulled out of) the current sprint.",
         "customfield_11732": "SIS-1564",
         "customfield_13330": {
             "value": "Katana",
             "id": "13611"
         },
-        "customfield_10013": 5.01
+        "customfield_10013": 8.01,
+        "customfield_10930": 2439
     }
 }
