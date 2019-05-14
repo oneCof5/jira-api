61ee38aa98a5ec3e64c11cee31c10ce05fadd38e
Chris Pearson <cpearson@silverchair.com>
2019-05-14 13:53:25 -0400

    examples of posting multiple issues via Jira API

---
+++ post-createIssues.js
@@ -0,0 +1,54 @@
+{
+    "issueUpdates": [
+        {
+            "update": {},
+            "fields": {
+                "project": {
+                    "id": "13280"
+                },
+                "summary": "Staging/Production Validation in Sprint 174 (Katana BAU)",
+                "issuetype": {
+                    "id": "3"
+                },
+                "reporter": {
+                    "name": "cpearson"
+                },
+                "labels": [
+                    "placeholder"
+                ],
+                "description": "h2. Staging and Production post validation\r\nTiming expectations: https://confluence.silverchair.com/display/RM/Validation+expectations+for+scheduled+posts\r\n\r\nSCM6 Validation Board - Katana: \r\nhttps://jira.silverchair.com/secure/RapidBoard.jspa?rapidView=331&quickFilter=2122\r\n\r\n|| Environment/Post || Date || Automated Tests || Validation Complete || Notes ||\r\n| Staging R1 |  |  |  |  |\r\n| Staging R2 |  |  |  |  |\r\n| Production |  |  |  |  |",
+                "customfield_11732": "SIS-1564",
+                "customfield_13330": {
+                    "value": "Katana",
+                    "id": "13611"
+                },
+                "customfield_10013": 5.01
+            }
+        },
+        {
+            "update": {},
+            "fields": {
+                "project": {
+                    "id": "13280"
+                },
+                "summary": "Staging/Production Validation in Sprint 174 (Katana OUP OP)",
+                "issuetype": {
+                    "id": "3"
+                },
+                "reporter": {
+                    "name": "cpearson"
+                },
+                "labels": [
+                    "placeholder"
+                ],
+                "description": "h2. Staging and Production post validation\r\nTiming expectations: https://confluence.silverchair.com/display/RM/Validation+expectations+for+scheduled+posts\r\n\r\nSCM6 Validation Board - Katana: \r\nhttps://jira.silverchair.com/secure/RapidBoard.jspa?rapidView=331&quickFilter=2122\r\n\r\n|| Environment/Post || Date || Automated Tests || Validation Complete || Notes ||\r\n| Staging R1 |  |  |  |  |\r\n| Staging R2 |  |  |  |  |\r\n| Production |  |  |  |  |",
+                "customfield_11732": "SIS-1564",
+                "customfield_13330": {
+                    "value": "Katana OUP OP",
+                    "id": "20525"
+                },
+                "customfield_10013": 5.01
+            }
+        }
+    ]
+}
