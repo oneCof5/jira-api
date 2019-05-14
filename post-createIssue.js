93b858fef9454fb24bbec6c376f25b9e430a0baf
Chris Pearson <cpearson@silverchair.com>
2019-05-14 13:17:26 -0400

    POST JSON examples

---
+++ post-createIssue.js
@@ -0,0 +1,35 @@
+// source values from https://jira.silverchair.com/rest/api/2/issue/createmeta
+{
+    "update": {
+        "worklog": [
+            {
+                "add": {
+                    "timeSpent": "1m",
+                    "started": "2019-05-14T13:00:00.000+0000"
+                }
+            }
+        ]
+    },
+    "fields": {
+        "project": {
+            "id": "13680" // SCMP
+        },
+        "summary": "Staging/Production Validation in Sprint XXX (Katana BAU)",
+        "issuetype": {
+            "id": "3"
+        },
+        "reporter": {
+            "name": "cpearson"
+        },
+        "labels": [
+            "placeholder"
+        ],
+        "security": {
+            "id": "10400"
+        },
+        "description": "h2. Staging and Production post validation\r\nTiming expectations: https://confluence.silverchair.com/display/RM/Validation+expectations+for+scheduled+posts\r\n\r\nSCM6 Validation Board - Katana: \r\nhttps://jira.silverchair.com/secure/RapidBoard.jspa?rapidView=331&quickFilter=2122\r\n\r\n|| Environment/Post || Date || Automated Tests || Validation Complete || Notes ||\r\n| Staging R1 |  |  |  |  |\r\n| Staging R2 |  |  |  |  |\r\n| Production |  |  |  |  |",
+        "customfield_11732": "SCMP-14599", // Epic Link
+        "customfield_13330": "13611", // Team Assigned
+        "customfield_10013": 5.01 // Story Points
+    }
+}
