2a627a1577fbd7e960bc69761e30722024e725ee
Chris Pearson <cpearson@silverchair.com>
2019-05-14 14:52:13 -0400

    added sprint

--- post-createIssues.js
+++ post-createIssues.js
@@ -1,30 +1,117 @@
 {
     "issueUpdates": [
-        {
-            "update": {},
-            "fields": {
-                "project": {
-                    "id": "13280"
-                },
-                "summary": "Staging/Production Validation in Sprint 174 (Katana BAU)",
-                "issuetype": {
-                    "id": "3"
-                },
-                "reporter": {
-                    "name": "cpearson"
-                },
-                "labels": [
-                    "placeholder"
-                ],
-                "description": "h2. Staging and Production post validation\r\nTiming expectations: https://confluence.silverchair.com/display/RM/Validation+expectations+for+scheduled+posts\r\n\r\nSCM6 Validation Board - Katana: \r\nhttps://jira.silverchair.com/secure/RapidBoard.jspa?rapidView=331&quickFilter=2122\r\n\r\n|| Environment/Post || Date || Automated Tests || Validation Complete || Notes ||\r\n| Staging R1 |  |  |  |  |\r\n| Staging R2 |  |  |  |  |\r\n| Production |  |  |  |  |",
-                "customfield_11732": "SIS-1564",
-                "customfield_13330": {
-                    "value": "Katana",
-                    "id": "13611"
-                },
-                "customfield_10013": 5.01
-            }
-        },
+      {
+          "update": {},
+          "fields": {
+              "project": {
+                  "id": "13280"
+              },
+              "summary": "[PLACEHOLDER] Unplanned Work in PI 8 Planning Sprint - Katana BAU",
+              "issuetype": {
+                  "id": "3"
+              },
+              "reporter": {
+                "name": "cpearson",
+                "key": "cpearson",
+                "emailAddress": "cpearson@silverchair.com"
+              },
+              "labels": [
+                  "placeholder",
+                  "platform_other"
+              ],
+              "description": "h2. Placholder ticket\r\nPoints to be consumed by surprise work pulled into (or added to with work pulled out of) the current sprint.",
+              "customfield_11732": "SIS-1564",
+              "customfield_13330": {
+                  "value": "Katana",
+                  "id": "13611"
+              },
+              "customfield_10013": 8.01
+          }
+      },
+      {
+          "update": {},
+          "fields": {
+              "project": {
+                  "id": "13280"
+              },
+              "summary": "[PLACEHOLDER] Unplanned Work in Sprint 174 - Katana BAU",
+              "issuetype": {
+                  "id": "3"
+              },
+              "reporter": {
+                "name": "cpearson",
+                "key": "cpearson",
+                "emailAddress": "cpearson@silverchair.com"
+              },
+              "labels": [
+                  "placeholder",
+                  "platform_other"
+              ],
+              "description": "h2. Placholder ticket\r\nPoints to be consumed by surprise work pulled into (or added to with work pulled out of) the current sprint.",
+              "customfield_11732": "SIS-1564",
+              "customfield_13330": {
+                  "value": "Katana",
+                  "id": "13611"
+              },
+              "customfield_10013": 8.01
+          }
+      },
+      {
+          "update": {},
+          "fields": {
+              "project": {
+                  "id": "13280"
+              },
+              "summary": "[PLACEHOLDER] Unplanned Work in Sprint 175 - Katana BAU",
+              "issuetype": {
+                  "id": "3"
+              },
+              "reporter": {
+                "name": "cpearson",
+                "key": "cpearson",
+                "emailAddress": "cpearson@silverchair.com"
+              },
+              "labels": [
+                  "placeholder",
+                  "platform_other"
+              ],
+              "description": "h2. Placholder ticket\r\nPoints to be consumed by surprise work pulled into (or added to with work pulled out of) the current sprint.",
+              "customfield_11732": "SIS-1564",
+              "customfield_13330": {
+                  "value": "Katana",
+                  "id": "13611"
+              },
+              "customfield_10013": 8.01
+          }
+      },
+      {
+          "update": {},
+          "fields": {
+              "project": {
+                  "id": "13280"
+              },
+              "summary": "[PLACEHOLDER] Unplanned Work in Sprint 176 - Katana BAU",
+              "issuetype": {
+                  "id": "3"
+              },
+              "reporter": {
+                "name": "cpearson",
+                "key": "cpearson",
+                "emailAddress": "cpearson@silverchair.com"
+              },
+              "labels": [
+                  "placeholder",
+                  "platform_other"
+              ],
+              "description": "h2. Placholder ticket\r\nPoints to be consumed by surprise work pulled into (or added to with work pulled out of) the current sprint.",
+              "customfield_11732": "SIS-1564",
+              "customfield_13330": {
+                  "value": "Katana",
+                  "id": "13611"
+              },
+              "customfield_10013": 8.01
+          }
+      },
         {
             "update": {},
             "fields": {
